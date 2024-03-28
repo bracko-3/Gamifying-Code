abcf2f830b711dea61d47d636f87751ff6bbf44d
bracko-3 <cummijb@mail.uc.edu>
2024-03-28 00:36:16 -0400

    refresh leaderboard every 5 seconds

--- Gamifying_Code/Assets/Scripts/LeaderboardManager.cs
+++ Gamifying_Code/Assets/Scripts/LeaderboardManager.cs
@@ -16,11 +16,26 @@ public class LeaderboardManager : MonoBehaviour
         leaderboardInfo = new List<List<object>>();
 
         // get leaderboard info
-        FirebaseAPI.GetUsers(gameCode, HandleTopUsers);
+        StartCoroutine(GetUsersEveryFiveSeconds());
+    }
+
+    IEnumerator GetUsersEveryFiveSeconds()
+    {
+        while (true)
+        {
+            // Call your function to get users
+            FirebaseAPI.GetUsers(gameCode, HandleTopUsers);
+
+            // Wait for 5 seconds
+            yield return new WaitForSeconds(5f);
+        }
     }
 
     private void HandleTopUsers(List<List<object>> topUsers)
     {
+        // clear list
+        leaderboardInfo.Clear();
+
         // Use the topUsers list here
         foreach (var userScorePair in topUsers)
         {
@@ -36,6 +51,7 @@ public class LeaderboardManager : MonoBehaviour
     {
         updateLeaderboard1();
         updateLeaderboard2();
+        updateLeaderboard3();
     }
     
     // [userName, totalQuestions, questionsCorrect, attacksLanded, gamifyScore]
@@ -82,5 +98,27 @@ public class LeaderboardManager : MonoBehaviour
         attacksLanded2.text = leaderboardInfo[1][3].ToString();
         quizPercentText2.text = $"{quizPercent1}%";
         gamifyScore2.text = leaderboardInfo[1][4].ToString();
+    }
+
+    // ########################################
+    // Leaderboard row 3
+    // ########################################
+    public Text place3;
+    public Text name3;
+    public Text attacksLanded3;
+    public Text quizPercentText3;
+    public Text gamifyScore3;
+    private int quizPercent3;
+
+    public void updateLeaderboard3()
+    {
+        int totalQuestions = Convert.ToInt32(leaderboardInfo[2][1]);
+        int questionsCorrect = Convert.ToInt32(leaderboardInfo[2][2]);
+        quizPercent3 = (int)Math.Round((float)questionsCorrect / totalQuestions * 100);
+        place3.text = "3";
+        name3.text = leaderboardInfo[2][0].ToString();
+        attacksLanded3.text = leaderboardInfo[2][3].ToString();
+        quizPercentText3.text = $"{quizPercent1}%";
+        gamifyScore3.text = leaderboardInfo[2][4].ToString();
     }
 }
