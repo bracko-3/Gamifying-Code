5ad48fad8c3b3522802f5a997e4e436788c6db2e
bracko-3 <cummijb@mail.uc.edu>
2024-03-28 00:06:11 -0400

    percent working

--- Gamifying_Code/Assets/Scripts/LeaderboardManager.cs
+++ Gamifying_Code/Assets/Scripts/LeaderboardManager.cs
@@ -30,6 +30,8 @@ public class LeaderboardManager : MonoBehaviour
         // Log the contents of leaderboardInfo as a JSON string
         string json = JsonConvert.SerializeObject(leaderboardInfo);
         Debug.Log($"from LEADERBOARDINFO: {json}");
+
+        updateLeaderboard();
     }
 
     void Update()
@@ -37,12 +39,25 @@ public class LeaderboardManager : MonoBehaviour
         
     }
     
+    // [userName, totalQuestions, questionsCorrect, attacksLanded, gamifyScore]
+
     // Static leaderboard
     public Text place1;
     public Text name1;
     public Text attacksLanded1;
-    public Text quizPercent1;
+    public Text quizPercentText1;
     public Text gamifyScore1;
+    private int quizPercent1;
 
-
+    public void updateLeaderboard() 
+    {
+        int totalQuestions = Convert.ToInt32(leaderboardInfo[0][1]);
+        int questionsCorrect = Convert.ToInt32(leaderboardInfo[0][2]);
+        quizPercent1 = (int)Math.Round((float)questionsCorrect / totalQuestions * 100);
+        place1.text = "1";
+        name1.text = leaderboardInfo[0][0].ToString();
+        attacksLanded1.text = leaderboardInfo[0][3].ToString();
+        quizPercentText1.text = $"{quizPercent1}%";
+        gamifyScore1.text = leaderboardInfo[0][4].ToString();
+    }
 }
