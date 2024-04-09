a4a4d79a8b97f4b66da3f1e177b896b7a545108a
Grant <grantchapel22@gmail.com>
2024-04-09 00:11:28 -0400

    fixing game

---
+++ Gamifying_Code/Assets/SpawnEnemyAttack.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemyAttack : MonoBehaviour
+{
+    public GameObject EnemyAttack1;
+    public GameObject EnemyAttack2;
+    public GameObject EnemyAttack3;
+
+   public void spawnattack1() { 
+
+    }
+    public void spawnattack2()
+    {
+
+    }
+    public void spawnattack3()
+    {
+
+    }
+
+
+
+}
